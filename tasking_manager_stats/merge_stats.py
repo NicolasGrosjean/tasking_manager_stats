2f9be5f4acc76b463b9a1d70ddf35f6dfd3597f3
NicolasGrosjean <grosjen@hotmail.fr>
2020-06-27 15:54:21 +0200

    Fix merge_stats
    - Support directories
    - Fix bad duration
    - Replace Jean-Yves Longchamp by JYL45

--- tasking_manager_stats/merge_stats.py
+++ tasking_manager_stats/merge_stats.py
@@ -7,8 +7,13 @@ if __name__ == '__main__':
     stats_dir = os.path.join(dm.get_data_dir(), 'stats')
     stats = pd.DataFrame()
     for file in os.listdir(stats_dir):
-        df = pd.read_csv(os.path.join(stats_dir, file), encoding='ISO-8859-1')
-        stats = pd.concat([stats, df], axis=0)
+        if file.endswith('.csv'):
+            df = pd.read_csv(os.path.join(stats_dir, file), encoding='ISO-8859-1')
+            stats = pd.concat([stats, df], axis=0)
+    stats['Duration'] = stats['Duration'].fillna(0)
+    stats.loc[stats['Duration'] > 7200, 'Duration'] = 7200
+    stats['Duration'] = stats['Duration'].astype(int)
+    stats.loc[stats['Author'] == 'Jean-Yves Longchamp', 'Author'] = 'JYL45'
     stats.to_csv(os.path.join(dm.get_data_dir(), 'merged_stats.csv'), index=None)
     stats_agg1 = stats.groupby(['Author', 'Type']).sum()['Duration']
     stats_agg2 = stats.groupby(['Author', 'Type']).count()['Task']
