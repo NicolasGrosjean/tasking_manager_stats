6ce12ae8562d56c74b83a84fe88617e481433495
NicolasGrosjean <grosjen@hotmail.fr>
2022-04-24 17:02:24 +0200

    Add date columns to merge building stats

--- merge_building_stats.py
+++ merge_building_stats.py
@@ -14,7 +14,12 @@ if __name__ == '__main__':
     for file in tqdm(os.listdir(buildings_dir)):
         if file.endswith('.csv'):
             df = pd.read_csv(os.path.join(buildings_dir, file))
-            df['BuildingsNbFromStart'] = df['BuildingNb'] - df.loc[0, 'BuildingNb']
+            df['BuildingDailyDiff'] = (df['BuildingNb'] - df['BuildingNb'].shift(1).fillna(0)).astype(int)
             df['Project'] = file[:-4]
             stats = pd.concat([stats, df], axis=0)
+    dates = pd.to_datetime(stats['Date'], format='%Y-%m-%d').dt
+    stats['Year'] = dates.year
+    stats['Month'] = dates.month
+    stats['Day'] = dates.day
+    del stats['Date']
     stats.to_csv(os.path.join(dm.get_data_dir(), 'merged_building_stats.csv'), index=None)
